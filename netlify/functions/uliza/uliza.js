23908c258fadf3c88fc39a3f898c730194e1af6f
vansoundz <thebeatplanet.silente@gmail.com>
2023-05-21 19:57:52 +0300

    make function work

--- netlify/functions/uliza/uliza.js
+++ netlify/functions/uliza/uliza.js
@@ -1,47 +1,43 @@
-const express = require('express');
-const cors = require('cors');
-const { spawn } = require('child_process');
-const serverless = require("serverless-http")
+const express = require("express");
+const cors = require("cors");
+const { spawn } = require("child_process");
+const serverless = require("serverless-http");
 
-const app = express();
-const port = 3000;
-
-app.use(express.json());
-app.use(cors());
-
-app.post('/askChatbot', (req, res) => {
-  const question  = req.body.userInput;
-  const pythonProcess = spawn('python3', ['uliza2.py']);
-  let answer = '';
-
-  console.log({question})
-
-  pythonProcess.stdout.on('data', (data) => {
-    answer += data.toString();
-    console.log(data, 'data');
-    console.log(answer, 'answer')
-  });
-
-  pythonProcess.stderr.on('error', (error) => {
-    console.log({error})
-    res.status(500).json({ error: error.toString() });
-  });
-
-  pythonProcess.on('close', () => {
-    console.log({answer})
-    res.json({ answer: answer.trim() });
-  });
-
-  pythonProcess.stdin.write(question + '\n');
-  pythonProcess.stdin.end();
-});
-
-
-// app.listen(port, () => {
-//   console.log(`Server running on http://localhost:${port}`);
-// });
 const handler = async (event, context) => {
-  return serverless(app)(event, context);
-}
+  const app = express();
+  app.use(express.json());
+  app.use(cors());
 
-module.exports = { handler }
+  const router = express.Router();
+
+  router.post('/uliza', (req, res) => {
+    const question = req.body.userInput;
+
+    const pythonProcess = spawn("python3", ["uliza2.py"]);
+    let answer = "";
+
+    pythonProcess.stdout.on("data", (data) => {
+      answer += data.toString();
+      console.log(data, "data");
+      console.log(answer, "answer");
+    });
+
+    pythonProcess.stderr.on("error", (error) => {
+      console.log({ error });
+      res.status(500).json({ error: error.toString() });
+    });
+
+    pythonProcess.on("close", () => {
+      res.json({ answer: answer.trim() });
+    });
+
+    pythonProcess.stdin.write(question + "\n");
+    pythonProcess.stdin.end();
+  });
+  app.use('/.netlify/functions/', router);
+console.log("SERVE")
+  return serverless(app)(event, context);
+
+};
+
+module.exports = { handler };
